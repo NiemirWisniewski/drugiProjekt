5643627aba9aa56a665ae2675b9da437dc22d651
Niemir Wisniewski <niemirwisniewski@gmail.com>
2022-10-04 06:51:58 +0200

    Hello kurwy change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello kurwy!!");
+        System.out.println("kuyrwy jade was na haxach");
     }
 }
